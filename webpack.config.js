1151fc324fffff8cf6907756add16a66a0f7e0da
Pedro Kayser <pedrokayser00@gmail.com>
2017-10-18 22:05:45 -0200

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const extract = require("extract-text-webpack-plugin")
+const css = new extract("./public/app.css")
+
+module.exports = {
+  entry: "./source/index.jsx",
+  output: {
+    filename: "./public/app.jsx"
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.[js|jsx]$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015", "react"]
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: css.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        }),
+      }
+    ]
+  },
+  plugins: [css]
+}
